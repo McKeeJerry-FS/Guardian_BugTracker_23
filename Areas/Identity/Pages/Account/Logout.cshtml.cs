fa832230d0e23ab1827d7d71a560e22488536c0a
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2024-01-02 12:13:25 -0500

    Logout function now works on the application

--- Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -36,7 +36,7 @@ namespace Guardian_BugTracker_23.Areas.Identity.Pages.Account
             {
                 // This needs to be a redirect so that the browser performs a new
                 // request and the identity for the user gets updated.
-                return RedirectToPage();
+                return RedirectToAction("Landing", "Home");
             }
         }
     }
